4663b04031353544545bec9830a07348bb09bb05
saravana2310 <saravanakumarskater@gmail.com>
2021-05-17 16:59:33 +0530

    My first code

---
+++ src/test/java/com/p1/Sample.java
@@ -0,0 +1,23 @@
+package com.p1;
+
+public class Sample {
+
+	public void eat(String name)
+	{
+		System.out.println("Sathya eat " +name);
+	}
+	public void sleep()
+	{
+		System.out.println("Sathya sleep with desi girls");
+	}
+	public static void main(String[] args) {
+		
+		Sample s = new Sample();
+		s.eat("manusan pee");
+		s.eat("Maatu pee");
+		s.eat("Naai pee");
+		s.eat("Kalutha pee");
+		s.sleep();
+		
+	}
+}
